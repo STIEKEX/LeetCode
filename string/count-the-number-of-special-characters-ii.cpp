ff837d6d058481554a34cd1cc14346b18df428aa
Deepak Farswan <devfarswan56@gmail.com>
2026-05-27 08:23:37 +0530

    LC-count-the-number-of-special-characters-ii [Runtime: N/A; Memory: N/A]

--- string/count-the-number-of-special-characters-ii.cpp
+++ string/count-the-number-of-special-characters-ii.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        int n = word.size() ; 
 
-       unordered_map<char ,vector<int>>m ; 
-
-       for(int i = 0 ; i<n ; i++){
-            
-            m[word[i]].push_back(i) ; 
-       }
-
-       int count = 0 ;      
+        vector<int>lower(26 , -1) ; 
+        vector<int>upper(26 , -1) ; 
 
 
+        for(int i = 0 ; i<word.size() ; i++){
 
-        for(int i = 0 ; i<n ; i++){
+            char ch = word[i] ; 
 
-            if(word[i] >= 'A' && word[i] <='Z'){
+            if(ch >= 'a' && ch <= 'z'){
+                 lower[ch - 'a'] = i ; 
+            }
 
-                char ch = tolower(word[i]) ; 
-                bool spec = true ; 
+            else{
 
-                if(m.find(ch) != m.end()){
-
-                    vector<int>&temp = m[ch] ; 
-
-                    for(int val : temp){
-                        if(val > i){
-                            spec = false ;
-                            break ; 
-                        }
-                    }
-                    if(spec)count++ ; 
+                if(upper[ch-'A'] == -1){
+                    upper[ch-'A'] = i ; 
                 }
-                
+            }
+        }
+
+
+        int count =0 ; 
+
+        for(int i = 0 ; i<26 ; i++){
+
+            if(lower[i] != -1 && upper[i] != -1){
+                if(lower[i] < upper[i]) count++ ; 
             }
         }
         return count ;
+
+
 
         
     }

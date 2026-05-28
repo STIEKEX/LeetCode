eed8b1dffdb2a883a4295c968947a83f01cdb3bf
Deepak Farswan <devfarswan56@gmail.com>
2026-05-29 00:15:33 +0530

    LC-buddy-strings [Runtime: N/A; Memory: N/A]

--- hash-table/buddy-strings.cpp
+++ hash-table/buddy-strings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool buddyStrings(string s, string goal) {
 
-        bool same = true  ;
+        bool same = false  ;
         char a = s[0] ;
         // for(char ch : s){
         //     if(ch != a){
@@ -17,17 +17,20 @@ public:
             m[ch]++ ; 
         }
 
-        for(auto val : m){
-            if(val.second % 2 == 1){
-                same = false ;
-                break ;
+        if(s == goal){
+            for(auto val : m){
+                if(val.second > 1){
+                    same = true ;
+                    break ;
+                }
             }
+            if(same) return true ;
+            return false ;
         }
-        if(same) return true ;
 
 
 
-        if(s == goal) return false; 
+        // if(s == goal) return false; 
         // sort(s.begin() , s.end()) ;
         // sort(goal.begin() , goal.end()); 
         // return s == goal ;

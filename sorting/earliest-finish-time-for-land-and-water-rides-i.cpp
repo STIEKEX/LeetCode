b63560c261834b0105678ae75fe081720eaad562
Deepak Farswan <devfarswan56@gmail.com>
2026-06-02 10:13:43 +0530

    LC-earliest-finish-time-for-land-and-water-rides-i [Runtime: N/A; Memory: N/A]

--- sorting/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ sorting/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,54 +1,34 @@
 class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        
-        // 6 9   9 
-
-        // 9 12  13 10
-
-
-
-        // int landSum = 0  ;
-        // int waterSum =  0 ;
 
         int m = landStartTime.size() ; 
         int n = waterStartTime.size() ; 
 
-        for(int i = 0 ; i<landDuration.size() ; i++){
-            landStartTime[i] += landDuration[i] ; 
-        }
-
-        for(int i = 0 ; i<waterDuration.size() ; i++){
-            waterStartTime[i] += waterDuration[i] ; 
-        }
-
-
         int ans = INT_MAX ; 
+        for(int i =0 ; i<m ; i++){
 
-        int i = 0 , j = 0 ; 
-
-        for(int i = 0 ; i<m ; i++){
-            int x = landStartTime[i] ; 
+            int f = landStartTime[i] + landDuration[i] ; 
 
             for(int j = 0 ; j<n ; j++){
-                int y = waterDuration[j] ; 
-                ans = min(ans , x + y) ; 
+                int start = max(waterStartTime[j] , f) ; 
+
+                ans = min(ans , start+waterDuration[j]) ; 
             }
         }
 
 
-        for(int i = 0 ; i<n ; i++){
-            int x = waterStartTime[i] ; 
+        for(int i =0 ; i<n ; i++){
+
+            int f = waterStartTime[i] + waterDuration[i] ; 
 
             for(int j = 0 ; j<m ; j++){
-                int y = landDuration[j] ; 
-                ans = min(ans , x + y) ; 
+                int start = max(landStartTime[j] , f) ; 
+
+                ans = min(ans , start+landDuration[j]) ; 
             }
         }
         return ans ;
-
         
-
-       
     }
 };

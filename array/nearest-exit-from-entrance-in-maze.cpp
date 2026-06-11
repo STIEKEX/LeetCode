e23ece8d3da9303c880cb0f461c61ca481bdacdf
Deepak Farswan <devfarswan56@gmail.com>
2026-06-11 14:16:43 +0530

    LC-nearest-exit-from-entrance-in-maze [Runtime: N/A; Memory: N/A]

--- array/nearest-exit-from-entrance-in-maze.cpp
+++ array/nearest-exit-from-entrance-in-maze.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
-    
-    int dfs(int i ,int j , int m , int n ,vector<vector<char>>&maze,  int I , int J  , vector<vector<bool>>&seen , int count){
-
-        if( i < 0 || j < 0 || i >= m || j >= n  || maze[i][j] == '+' || seen[i][j]) return 1e9 ; 
-
-        if(i == 0 || j == 0 || i == m-1 || j == n-1){
-           if(i != I || j != J){
-             
-            return 0; 
-           }
-        }
-
-        seen[i][j] = true ; 
-        int down = 1 + dfs(i+1 , j , m , n , maze , I , J , seen , count+1) ;
-        int top = 1 +  dfs(i-1 , j , m , n , maze , I , J , seen , count + 1) ; 
-       int left =  1 + dfs(i , j+1 , m , n ,  maze , I , J , seen , count + 1 ) ; 
-       int right = 1 +  dfs(i , j-1 , m , n , maze , I , J  , seen , count + 1) ; 
-       seen[i][j] = false ; 
-        return min({top , down , left , right}) ; 
-
-      
-    }
     int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
 
         int m = maze.size() ; 
         int n = maze[0].size() ; 
 
-        int srcI = e[0] ; 
-        int srcJ = e[1] ; 
-        int count = 0 ;
-        vector<vector<bool>>seen(m , vector<bool>(n , false)) ;
-       int ans =  dfs(srcI , srcJ , m , n , maze , srcI , srcJ ,seen , count) ;
 
-       if(ans >= 1e9) return -1 ; 
-       return ans ; 
-       
+        int i = e[0] ; 
+        int j = e[1] ; 
 
 
+        queue<pair<pair<int ,int> , int>>q ; 
+        vector<vector<bool>>seen(m , vector<bool>(n , false)) ; 
 
+        q.push({{i , j } , 0}) ; 
 
+        seen[i][j] = true ; 
+
+        while(!q.empty()){
+
+            int i = q.front().first.first;  
+            int j = q.front().first.second ; 
+            int dis = q.front().second ;
+            q.pop() ; 
+            seen[i][j] = true ; 
+            if(i == 0 || j == 0 || i == m-1 || j == n-1 ){
+                if(i != e[0] || j != e[1]) {
+                    return dis ; 
+                }
+
+            }
+
+            if(i+1 < m && seen[i+1][j] != true && maze[i+1][j] != '+'){
+                q.push({{i+1 , j} , dis+1}) ; 
+            }
+            if(j+1 < n  && seen[i][j+1] != true && maze[i][j+1] != '+'){
+                q.push({{i , j+1} , dis+1}) ; 
+            }
+            if(i-1 >= 0 && seen[i-1][j] != true && maze[i-1][j] != '+'){
+                q.push({{i-1 , j} , dis+1}) ; 
+            }
+            if(j-1 >= 0 && seen[i][j-1] != true && maze[i][j-1] != '+'){
+                q.push({{i, j-1} , dis+1}) ; 
+            }
+
+        }
+        return -1 ; 
         
     }
 };

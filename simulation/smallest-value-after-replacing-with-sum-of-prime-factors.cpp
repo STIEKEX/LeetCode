7f15775f9beb204bfe2c29aeddf509819de10b86
Deepak Farswan <devfarswan56@gmail.com>
2026-06-12 18:06:25 +0530

    LC-smallest-value-after-replacing-with-sum-of-prime-factors [Runtime: N/A; Memory: N/A]

---
+++ simulation/smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestValue(int n) {
+        if(n == 4) return n ; 
+        
+
+       while(true){
+        int o = n ; 
+        int sum = 0 ;
+        for(int i = 2 ; i*i <=n ; i++){
+            while(n % i == 0){
+                sum += i ; 
+                n = n/i ; 
+            }
+        }
+        if(n > 1){
+            sum += n ; 
+        }
+        if(sum == o) return sum  ; 
+       
+       }
+       return -1 ; 
+        
+    }
+};

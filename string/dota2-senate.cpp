ab892a68c9035ceacdff349e811041c1ccf42ee8
Deepak Farswan <devfarswan56@gmail.com>
2026-06-12 08:21:52 +0530

    LC-dota2-senate [Runtime: N/A; Memory: N/A]

--- string/dota2-senate.cpp
+++ string/dota2-senate.cpp
@@ -5,19 +5,21 @@ public:
         queue<char>q ; 
         int n = s.size() ; 
 
-
+        int count = 0 ; 
         for(char  ch : s){
             q.push(ch) ; 
         }
         while(q.size() != 1){
+            if(count > 1000) break ; 
             char a = q.front() ;
             q.pop() ; 
 
             char b = q.front() ; 
             if(a != b){
-                q.pop() ; 
-                 
+                q.pop() ;
+                count-- ;   
             }
+            count++ ; 
             q.push(a) ; 
         }
         if(q.front() == 'R'){

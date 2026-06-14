7585b6034e30a402b3bd4472067fbdc5446c925d
Deepak Farswan <devfarswan56@gmail.com>
2026-06-14 23:14:54 +0530

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

--- prefix-sum/product-of-array-except-self.cpp
+++ prefix-sum/product-of-array-except-self.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size() ;
-        vector<int>ans(n,1) ;
+        // Product of array except itself = product of everything on my left × product of everything on my right
 
-        for(int i= 1 ; i<n ; i++){
-            ans[i] = nums[i-1] * ans[i-1] ;
-        }
-        int suffix = 1 ;
-        for(int i = n-2 ; i>=0 ; i--){
-            suffix *= nums[i+1] ;
-            ans[i] *= suffix ;
-        }
-        return ans ; 
+        int n = nums.size() ; 
+        vector<int>ans(n , 1) ; 
 
-        
+        for(int i = 1 ; i<n ; i++){
+            ans[i] = ans[i-1] * nums[i-1] ; 
+        }
+
+        int suf = 1 ;
+        for(int j = n-2 ; j>=0 ; j--){
+            suf *= nums[j+1] ; 
+            ans[j] *= suf ; 
+        }
+        return ans ;
         
     }
 };

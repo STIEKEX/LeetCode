a078560ea378ccff3806043c35b4d83d0fdd2a63
Deepak Farswan <devfarswan56@gmail.com>
2026-06-16 15:06:38 +0530

    LC-construct-target-array-with-multiple-sums [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/construct-target-array-with-multiple-sums.cpp
+++ heap-priority-queue/construct-target-array-with-multiple-sums.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     bool isPossible(vector<int>& target) {
         int n = target.size() ; 
-
+        bool is = false ;
+        int x = target[0] ; 
         for(int val : target){
             if(val > 1 && val < n){
                 return false ;
             }
+            if(val != x){
+                is = true ; 
+            }
             
         }
 
+        if(!is) return false ; 
         return true; 
         
     }

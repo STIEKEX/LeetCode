e3dfff6aab227ce27965d2552fcd171ea9dd1c28
Deepak Farswan <devfarswan56@gmail.com>
2026-07-03 10:54:43 +0530

    LC-network-recovery-pathways [Runtime: N/A; Memory: N/A]

--- array/network-recovery-pathways.cpp
+++ array/network-recovery-pathways.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>paths ; 
-    vector<int>arr; 
-    void dfs(int src , vector<vector<pair<int ,int>>>&adj , vector<bool>&online , long long k , int sum  ){
+   
+    int ans = INT_MIN ; 
+    void dfs(int src , vector<vector<pair<int ,int>>>&adj , vector<bool>&online , long long k , int sum , int mn ){
         
         if(!online[src] || sum > k  )return ;
         if(src == adj.size() -1) {
-           paths.push_back(arr) ; 
-           return ;
+           ans = max(ans , mn) ; 
            
         }
         for(auto val : adj[src]){
             int v = val.first ; 
             int c = val.second ;
+             
             
-            arr.push_back(c)  ;
-            dfs(v , adj , online , k, sum + c ) ; 
-            arr.pop_back() ;
+            dfs(v , adj , online , k, sum + c , min(mn ,c) ) ; 
+            
      
         }
     }   
@@ -33,24 +32,9 @@ public:
 
                 adj[u].push_back({v , c}) ;
             }
-            dfs(0 , adj , online , k , 0) ; 
-
-            vector<int>ans ; 
-
-            for(auto val : paths){
-                int mn  = INT_MAX ; 
-                for(int x : val){
-                    mn = min(x , mn) ; 
-                }
-                ans.push_back(mn) ; 
-            }
-            int mx = INT_MIN ; 
-
-            for(int val : ans){
-                mx = max(val , mx) ; 
-            }
-            if(mx == INT_MIN) return -1; 
-            return mx ;
+            dfs(0 , adj , online , k , 0 , INT_MAX) ; 
+            if(ans == INT_MIN) return -1 ;
+           return ans ;
 
             
 

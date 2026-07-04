c7658d92841d4f72ff1482f8fe56d3ae7c6fd5a5
Deepak Farswan <devfarswan56@gmail.com>
2026-07-04 10:48:39 +0530

    LC-minimum-score-of-a-path-between-two-cities [Runtime: N/A; Memory: N/A]

--- graph/minimum-score-of-a-path-between-two-cities.cpp
+++ graph/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
+     int mn = INT_MAX ; 
+     void dfs(int src , vector<vector<pair<int ,int>>>&adj , vector<bool>&seen ){
+        
+        if(seen[src] ) return  ;
+
+        seen[src] = true ; 
+
+        for(auto val : adj[src]){
+            int v = val.first ; 
+            int d = val.second ; 
+            mn = min(d , mn) ; 
+            dfs(v , adj , seen) ; 
+            
+        }
+     }
     int minScore(int n, vector<vector<int>>& roads) {
-            int mn = INT_MAX ; 
+           
+          
 
-            for(auto val : roads){
-                int dis = val[2] ; 
+           vector<vector<pair<int ,int>>>adj(n+1) ; 
+           for(auto val : roads){
+                int u = val[0] ; 
+                int v = val[1] ; 
+                int d = val[2] ; 
 
-                mn = min(dis , mn) ; 
-            }
-            return mn ; 
+                adj[u].push_back({v ,d}) ; 
+                adj[v].push_back({u ,d}) ; 
+           }
+
+           vector<bool>seen(n+1 , false) ; 
+
+            dfs(1 , adj , seen) ; 
+            return mn ;
+           
+
+
+
     }
 };

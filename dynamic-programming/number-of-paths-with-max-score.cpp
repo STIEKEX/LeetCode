fc97ead73e368554a1bd56e4780de9df40c151f4
Deepak Farswan <devfarswan56@gmail.com>
2026-07-06 10:07:58 +0530

    LC-number-of-paths-with-max-score [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/number-of-paths-with-max-score.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7 ; 
+    vector<int> pathsWithMaxScore(vector<string>& board) {
+        int n = board.size() ; 
+
+        vector<vector<int>>score(n , vector<int>(n , -1)) ; 
+
+        vector<vector<int>>ways(n , vector<int>(n , 0)) ; 
+
+
+        score[n-1][n-1] = 0  ;
+        ways[n-1][n-1] = 1 ; 
+
+        int dx[] = {1 , 0 , 1} ; 
+        int dy[] = {0 , 1 , 1} ;
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int j = n-1 ; j>=0 ; j--){
+
+                if(i == n-1 && j == n-1)continue ; 
+
+                if(board[i][j] == 'X')continue ; 
+
+                int best = -1 ; 
+                int cnt = 0 ; 
+
+                for(int k = 0 ; k<3 ; k++){
+                    int nx = i+ dx[k] ; 
+                    int ny = j + dy[k] ; 
+
+                    if(nx >=n ||  ny >=n) continue ; 
+
+                    if(score[nx][ny] == -1)continue ; 
+
+                    if(score[nx][ny] > best){
+                        best = score[nx][ny] ; 
+                        cnt = ways[nx][ny] ; 
+                    }
+                    else if(score[nx][ny] == best){
+                        cnt = (cnt + ways[nx][ny])%MOD ; 
+                    }
+                }
+
+                if(best == -1) continue ; 
+
+                int val = 0 ; 
+                if(isdigit(board[i][j])){
+                    val = board[i][j] - '0' ; 
+                }
+
+                score[i][j] = best +val ; 
+                ways[i][j] = cnt ; 
+
+
+            }
+        }
+        if(score[0][0] == -1)return {0 , 0} ; 
+        return {score[0][0] , ways[0][0]} ; 
+
+
+        
+    }
+};

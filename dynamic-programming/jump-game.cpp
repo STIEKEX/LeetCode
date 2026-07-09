365e413b5b102a07a9dfe61cb01614a2f782ac5c
Deepak Farswan <devfarswan56@gmail.com>
2026-07-10 01:05:19 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- dynamic-programming/jump-game.cpp
+++ dynamic-programming/jump-game.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int mx = 0 ; 
-        for(int i = 0 ; i<nums.size() ; i++){
-            if(i > mx) return false ;
-            mx =max(mx , i + nums[i]) ; 
+    bool solve(int i , vector<int>&nums){
+        
+        if(i == nums.size()-1) return true ;
+
+        for(int j = 1 ; j<=nums[i] ; j++){
+            if(i+j < nums.size()){
+
+                if(solve(i+j , nums)) return true ;
+            }
         }
-        return true ;
+        return false ;
+    }
+    bool canJump(vector<int>& nums) {
+        return solve(0 , nums) ; 
         
     }
 };

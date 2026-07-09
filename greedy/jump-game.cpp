d9dc9294fb81e49798bfe74ce84cfd0fe5a070c2
Deepak Farswan <devfarswan56@gmail.com>
2026-07-10 01:07:57 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- greedy/jump-game.cpp
+++ greedy/jump-game.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    vector<int>dp ; 
     bool solve(int i , vector<int>&nums){
         
         if(i == nums.size()-1) return true ;
-
+        if(dp[i] != -1) return dp[i] ; 
         for(int j = 1 ; j<=nums[i] ; j++){
             if(i+j < nums.size()){
 
-                if(solve(i+j , nums)) return true ;
+                if(solve(i+j , nums)){
+                    return dp[i] = true ;
+                }
             }
         }
-        return false ;
+        return dp[i] = false ;
     }
     bool canJump(vector<int>& nums) {
+        dp.resize(nums.size() +1 , -1) ;  
         return solve(0 , nums) ; 
         
     }

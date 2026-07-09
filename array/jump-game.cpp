7957888fdc4a97afacb8e6a25061f30c8d63dd02
Deepak Farswan <devfarswan56@gmail.com>
2026-07-10 00:55:40 +0530

    LC-jump-game [Runtime: 0 ms; Memory: 52.5 MB]

--- array/jump-game.cpp
+++ array/jump-game.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-
     bool canJump(vector<int>& nums) {
-        int n =  nums.size() ; 
-        int idx = 0 ;
-        int op = 0 ; 
-
-        int i = 0 ; 
-        while(i < n){
-            if(op == 100000) return false ;
-            if(i == n-1) return true ; 
-            i += nums[i];
-            op++ ; 
-
+        int mx = 0 ; 
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(i > mx) return false ;
+            mx =max(mx , i + nums[i]) ; 
         }
-        return false ;
-        
+        return true ;
         
     }
 };

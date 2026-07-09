557430a15989d07e1950489265254e15ac58e98a
Deepak Farswan <devfarswan56@gmail.com>
2026-07-09 11:50:45 +0530

    LC-path-existence-queries-in-a-graph-i [Runtime: N/A; Memory: N/A]

--- graph/path-existence-queries-in-a-graph-i.cpp
+++ graph/path-existence-queries-in-a-graph-i.cpp
@@ -4,24 +4,27 @@ public:
         // sort(queries.begin() , queries.end()) ; 
         vector<bool>ans; 
 
-        // vector<int> a(n+1 , 0) ; 
+        vector<int>comp(n , true) ; 
+
+        for(int i = 1 ; i<n ; i++){
+            if(abs(nums[i-1] - nums[i]) <= maxDiff){
+                comp[i-1] = true ;
+            }
+            else{
+                comp[i-1] = false ;
+            }
+        }
 
         
 
         for(auto val : queries){
             int u = val[0] ; 
             int v = val[1] ; 
-            bool is = true ;
-            for(int i = min(v ,u) ; i< max(v ,u) ; i++){
-                if(abs(nums[i] - nums[i+1]) > maxDiff){
-                    ans.push_back(false) ;
-                    is = false;  
-                    break ; 
-                }
-                
-            }
-            if(is){
+            if(comp[u] == comp[v]){
                 ans.push_back(true) ; 
+            }
+            else{
+                ans.push_back(false) ; 
             }
         }
         return ans ;

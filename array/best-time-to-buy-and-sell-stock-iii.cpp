40fabb0eff1fac02a60fd7b90dee09c0295f190e
Deepak Farswan <devfarswan56@gmail.com>
2026-07-15 00:41:08 +0530

    LC-best-time-to-buy-and-sell-stock-iii [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock-iii.cpp
+++ array/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>>dp ; 
+    vector<vector<vector<int>>>dp ;  
     vector<int>p ; 
 
     int solve(int i , int b , int t){
         if(i == p.size())return 0 ; 
 
-        if(dp[i][b] != -1) return dp[i][b] ;
+        if(dp[i][b][t] != -1) return dp[i][b][t] ;
 
         
         if(b){
             int taken = 0 ;
             if(t < 2){
-                taken = -p[i] + solve(i+1 , 0 , t+1)  ; 
+                taken = -p[i] + solve(i+1 , 0 , t)  ; 
 
             }
             int notTaken = solve(i+1 , 1 , t) ; 
-            return dp[i][b] = max(taken , notTaken) ; 
+            return dp[i][b][t] = max(taken , notTaken) ; 
 
            
         }
-        return dp[i][b] = max(p[i] + solve(i+1 , 1 , t) , solve(i+1 , 0 , t)) ; 
+        return dp[i][b][t] = max(p[i] + solve(i+1 , 1 , t+1) , solve(i+1 , 0 , t)) ; 
 
 
         
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size() ; 
-        dp.resize(n , vector<int>(2 , -1)) ; 
-        p = prices ; 
+        dp.resize(n , vector<vector<int>>(2 , vector<int>(3 ,-1)));
+        p = prices ;  
 
         return solve(0 , 1 , 0) ; 
 

c377c3c1a980de09b2bc17b871909d09bc22b202
Deepak Farswan <devfarswan56@gmail.com>
2026-07-15 10:09:48 +0530

    LC-gcd-of-odd-and-even-sums [Runtime: N/A; Memory: N/A]

--- number-theory/gcd-of-odd-and-even-sums.cpp
+++ number-theory/gcd-of-odd-and-even-sums.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int odd = n*n ; 
-        int even = n*(n+1) ; 
-        return __gcd(odd ,even) ; 
+        return n ;  
         
     }
 };

97174061991def85041ff88cdedbe8528fef493e
Deepak Farswan <devfarswan56@gmail.com>
2026-07-15 10:09:01 +0530

    LC-gcd-of-odd-and-even-sums [Runtime: N/A; Memory: N/A]

--- math/gcd-of-odd-and-even-sums.cpp
+++ math/gcd-of-odd-and-even-sums.cpp
@@ -1,28 +1,9 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-       int sumEven = n*(n+1)  ;
-        int sumOdd = n*n ; 
-        int gcd = 1 ;
-        if(sumEven == 0){
-            gcd = sumOdd ;
-        }
-        if(sumOdd== 0){
-            gcd = sumOdd ;
-        }
-        if(sumOdd == sumEven){
-            gcd = sumOdd ;
-        }
-
-        for(int i =1 ; i<min(sumEven, sumOdd) ; i++){
-            if(sumEven%i==0 && sumOdd %i==0){
-                gcd = i ; 
-            }
-        }
-        return gcd ;
-        
-        
-      
+        int odd = n*n ; 
+        int even = n*(n+1) ; 
+        return __gcd(odd ,even) ; 
         
     }
 };

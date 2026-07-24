3b93276ff5f2d25c6bbc71057a04a7c26765dc4c
Deepak Farswan <devfarswan56@gmail.com>
2026-07-24 20:41:16 +0530

    LC-triangle [Runtime: N/A; Memory: N/A]

--- dynamic-programming/triangle.cpp
+++ dynamic-programming/triangle.cpp
@@ -4,14 +4,16 @@ public:
     int solve(int i , int idx , vector<vector<int>>&t){
         if(idx == t.size()) return 0 ; 
 
+        if(dp[idx][i] != INT_MAX) return dp[idx][i] ; 
+
         int left = solve(i , idx+1 , t) ; 
         int right = solve(i+1  , idx + 1 , t) ; 
 
-        return min(left + t[idx][i] , right+t[idx][i]) ; 
+        return dp[idx][i] = min(left + t[idx][i] , right+t[idx][i]) ; 
     }
     int minimumTotal(vector<vector<int>>& t) {
         int n = t.size() ; 
-
+        dp.resize(n+1 , vector<int>(n+1 ,   INT_MAX)) ; 
         return solve(0 , 0 , t) ; 
        
 

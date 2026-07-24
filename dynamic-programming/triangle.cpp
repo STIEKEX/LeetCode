2d7e7ef020a8ba281606362e42ecd0d5d9f15aa5
Deepak Farswan <devfarswan56@gmail.com>
2026-07-24 20:37:11 +0530

    LC-triangle [Runtime: N/A; Memory: N/A]

--- dynamic-programming/triangle.cpp
+++ dynamic-programming/triangle.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int sum = 0 ; 
+    vector<vector<int>>dp ; 
+    int solve(int i , int idx , vector<vector<int>>&t){
+        if(idx == t.size()) return 0 ; 
 
-        for(auto val : triangle){
-            int mn = INT_MAX ; 
-            for(int num : val){
-                mn = min(num , mn) ; 
-            }
-            sum += mn ; 
-        }
-        return sum ; 
+        int left = solve(i , idx+1 , t) ; 
+        int right = solve(i+1  , idx + 1 , t) ; 
+
+        return min(left + t[idx][i] , right+t[idx][i]) ; 
+    }
+    int minimumTotal(vector<vector<int>>& t) {
+        int n = t.size() ; 
+
+        return solve(0 , 0 , t) ; 
+       
+
+
+        
         
     }
 };

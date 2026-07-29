f2ce0007615d95c5166790377df4713733c532df
Deepak Farswan <devfarswan56@gmail.com>
2026-07-29 20:40:07 +0530

    LC-word-break [Runtime: N/A; Memory: N/A]

--- memoization/word-break.cpp
+++ memoization/word-break.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& d) {
-        unordered_map<string ,int>m ; 
-        for(string val : d){
-            m[val]++ ; 
-        }
+    bool solve(int i , string s , vector<string>&d){
+        if(i == s.size()) return true ;
 
         string temp = "" ; 
-        for(char ch : s){
-            temp += ch ; 
-            if(m.find(temp) != m.end()){
-                temp = "" ; 
-            }
+        for(int j = i ; j<s.size() ; j++){
+            temp += s[j] ; 
 
+            if(find(d.begin() , d.end() , temp) != d.end() && solve(j+1 , s , d)){
+                return true ;
+            }
         }
-        if(temp != ""){
-            return false; 
-        }
-        return true ;
+        return false ;
+    }
+    bool wordBreak(string s, vector<string>& d) {
+        return solve(0 , s  ,d) ; 
+        
+       
         
     }
 };

e1c219eac16a302bbfabb337e199322920eb4776
Deepak Farswan <devfarswan56@gmail.com>
2026-07-30 20:09:18 +0530

    LC-minimum-number-of-pushes-to-type-word-i [Runtime: N/A; Memory: N/A]

--- string/minimum-number-of-pushes-to-type-word-i.cpp
+++ string/minimum-number-of-pushes-to-type-word-i.cpp
@@ -6,12 +6,19 @@ public:
 
         if(n <= 8) return n ; 
 
-        else if(n == 16) return 24 ; 
+        else if(n > 8 && n <= 16){
+            int rem = n-8 ; 
+            return 8 + (rem * 2) ; 
+        }
 
+        else if(n > 16 && n <= 24){
+            int rem = n-16 ; 
+            return 24 + (rem * 3) ; 
+        }
         else if(n == 26) return 8 + 16 + 24 + 8 ; 
-
-        int rem = n - 8 ; 
-        return 8 + (rem * 2) ; 
+        return 8 + 16 + 24 + 4 ; 
+          
+        
         
     }
 };

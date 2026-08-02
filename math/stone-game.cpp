cdf8d219655f21cc0bec8095551f8ae9de8bb5fc
Deepak Farswan <devfarswan56@gmail.com>
2026-08-02 09:08:38 +0530

    LC-stone-game [Runtime: N/A; Memory: N/A]

--- math/stone-game.cpp
+++ math/stone-game.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
+    vector<vector<int>>dp ; 
+    int solve(int i  , int j , vector<int>&p){
+        if(i == j) return p[i] ; 
+        if(dp[i][j] != -1) return dp[i][j] ; 
+        int left = p[i] - solve(i+1 , j , p) ;
+        int right = p[j] - solve(i , j-1 , p)  ;
+
+        return dp[i][j] = max(left , right)  ;
+    }
     bool stoneGame(vector<int>& piles) {
-        return true ;
+        int n = piles.size() ; 
+        dp.resize(n+1 ,vector<int>(n+1 , -1)) ; 
+
+        return solve(0 , n-1 , piles) >= 0 ; 
         
     }
 };

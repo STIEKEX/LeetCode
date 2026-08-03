f5e410ea8a0fc8c69451663aba9cebe2649176a2
Deepak Farswan <devfarswan56@gmail.com>
2026-08-03 20:53:21 +0530

    LC-stone-game-iii [Runtime: N/A; Memory: N/A]

--- game-theory/stone-game-iii.cpp
+++ game-theory/stone-game-iii.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    vector<int>dp ; 
-    int n = 0 ; 
-    int solve(int i , vector<int>&nums){
-        if(i >= n) return 0 ; 
-
-        if(dp[i] != INT_MIN) return dp[i] ; 
-
-        int one = nums[i] - solve(i+1 , nums) ; 
-        int two = INT_MIN ; 
-        int three = INT_MIN ;
-        if(i+1 < n){
-           two =  nums[i] + nums[i+1] - solve(i+2 , nums) ; 
-        }
-        if(i+2 < n){
-           three = nums[i] + nums[i+1] + nums[i+2] - solve(i+3  , nums) ; 
-        }
-        return dp[i] = max({one , two , three}) ; 
-    }
     string stoneGameIII(vector<int>& stone) {
-        n = stone.size() ; 
+         int n = stone.size() ; 
 
-        dp.resize(n+1 , INT_MIN) ; 
+        vector<int>dp(n+3 , 0) ; 
 
-         int val = solve(0 , stone) ; 
+        for(int i = n-1 ; i>=0 ; i--){
 
-         if(val >0) return "Alice" ; 
-         else if(val < 0) return "Bob" ; 
-         
-         return "Tie" ; 
+            int first = stone[i] - dp[i+1] ; 
+            int second = INT_MIN  ;
+            int third = INT_MIN ; 
+            if(i+1 < n){
+                second = stone[i] + stone[i+1] - dp[i+2] ; 
+            }
+            if(i+2 < n){
+                third = stone[i] + stone[i+1] + stone[i+2] - dp[i+3] ; 
+            }
+            dp[i] = max({first , second , third}) ;
 
+              
+        }
 
+        if(dp[0] > 0) return "Alice" ; 
+        else if(dp[0
+        ] < 0) return "Bob" ; 
+        return "Tie" ; 
         
     }
 };

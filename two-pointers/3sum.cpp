d4628ce0a8ed770c4e766d68678f7a7ede81687c
Deepak Farswan <devfarswan56@gmail.com>
2026-08-10 22:38:32 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        sort(nums.begin() , nums.end()) ;
+        sort(nums.begin() , nums.end()) ; 
         int n = nums.size() ; 
 
-        vector<vector<int>> ans ;
+        vector<vector<int>>ans ; 
 
-        for(int i= 0 ; i<n ; i++){
-            if(i>0 && nums[i] == nums[i-1])continue ;
-            int j = i +1 , k = n -1 ; 
-            
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum > 0){
-                    k-- ;
-                }else if (sum < 0){
-                    j++ ;
-                }else{
-                    ans.push_back({nums[i], nums[j], nums[k]}) ; 
-                  while (j < k && nums[j] == nums[j + 1]) j++;
-                  while (j < k && nums[k] == nums[k - 1]) k--;
-                  j++ , k-- ;
+        // int left = 0 ; 
+        // int right = n-1 ; 
 
+        for(int i = 0 ; i<n-2 ; i++){
+
+           
+            if(i > 0 && nums[i] == nums[i-1])continue ; 
+            int left = i+1 ; 
+            int right = n-1 ; 
+            while(left < right){
+
+                long long sum = nums[i] + nums[left] + nums[right] ; 
+                if(sum == 0) {
+                    ans.push_back({nums[i] , nums[left] , nums[right]}) ; 
+
+                    while(left < right && nums[left] == nums[left+1])left; 
+
+                    while(left < right && nums[right] == nums[right-1]) right ; 
+                    left++ ; 
+                    right-- ; 
                 }
+                else if(sum > 0) {
+                    right-- ;
+                }
+                else{
+                    left++ ; 
+                }
+
             }
         }
         return ans ;
-
         
     }
 };

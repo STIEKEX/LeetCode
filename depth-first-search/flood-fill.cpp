d9c87936806140566e3c48881e5dec446a558dff
Deepak Farswan <devfarswan56@gmail.com>
2026-08-14 23:17:08 +0530

    LC-flood-fill [Runtime: N/A; Memory: N/A]

--- depth-first-search/flood-fill.cpp
+++ depth-first-search/flood-fill.cpp
@@ -1,30 +1,26 @@
 class Solution {
-public:    
-        void dfs(int i , int j ,int m , int n , vector<vector<int>>& image ,int color , int og){
-            if(i < 0 || j < 0 || i>=m || j>= n || image[i][j] == color || image[i][j] != og) return ;
-            image[i][j] = color ;
-            
-            dfs(i-1 , j , m , n , image , color, og) ; 
-            dfs(i+1 , j , m , n , image , color, og) ; 
-            dfs( i , j+1 , m , n , image ,color, og) ; 
-            dfs(i , j-1  , m , n , image , color, og) ; 
+public:
+    vector<vector<bool>>seen ; 
+    void dfs(int i , int j , int m , int n  , vector<vector<int>>&nums , int color , int og){
+        if(i < 0 || j < 0 || i>= m || j >= n || seen[i][j] || nums[i][j] != og) return ;
 
-            
-        }
 
+        nums[i][j] = color ; 
+        seen[i][j] = true ;
+        dfs(i+1 , j , m , n ,  nums , color , og) ; 
+        dfs(i , j+1 , m , n ,  nums , color , og) ; 
+        dfs(i , j-1 , m , n ,  nums,  color , og) ; 
+        dfs(i-1 , j , m , n ,  nums,  color , og) ; 
+        return ;
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-
+        if(image[sr][sc] == color) return image ;
         int m = image.size() ; 
         int n = image[0].size() ; 
         int og = image[sr][sc] ; 
-
-        
-        dfs(sr , sc , m , n , image , color, og) ; 
-                
-         
-        
+        seen.resize(m , vector<bool>(n , false)) ; 
+        dfs(sr , sc , m , n , image , color , og) ; 
         return image ; 
-
         
     }
 };

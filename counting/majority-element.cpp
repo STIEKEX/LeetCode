a472d0d5f2c508a4dccb322a67df2fa0c3a76c1b
Deepak Farswan <devfarswan56@gmail.com>
2026-08-14 08:18:31 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- counting/majority-element.cpp
+++ counting/majority-element.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n= nums.size() ; 
-        unordered_map<int, int> m ;
-        for(int val : nums) {
-            m[val]++ ;
-        }
-        int ans= 0;
-        for(auto no : m){
-            if(no.second > n/2) {
-                ans = no.first ;
+
+        int ans = nums[0]; 
+
+        for(int i = 1 ; i<nums.size() ; i++){
+            if(nums[i] != ans){
+                ans = nums[i] ;
             }
         }
-        
         return ans ;
+        
     }
 };

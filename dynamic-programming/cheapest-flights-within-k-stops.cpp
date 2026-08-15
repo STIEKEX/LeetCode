d24cc61c3ee00326c8f1d5ef94166c4a10749c6e
Deepak Farswan <devfarswan56@gmail.com>
2026-08-15 11:53:38 +0530

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

--- dynamic-programming/cheapest-flights-within-k-stops.cpp
+++ dynamic-programming/cheapest-flights-within-k-stops.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& e, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
 
-        vector<vector<pair<int ,int>>>adj(n) ; 
+            vector<vector<pair<int ,int>>>adj(n) ; 
 
-        for(auto val : e){
-            int u = val[0] ;
-            int v = val[1] ; 
-            int w = val[2] ; 
+            for(auto val : flights){
+                int  u = val[0] ; 
+                int v = val[1] ; 
+                int w = val[2] ; 
 
-            adj[u].push_back({v ,w}) ; 
-        }
+                adj[u].push_back({v, w}) ; 
+            } 
+
+            vector<int>dis(n , INT_MAX) ; 
+
+           queue< pair< pair<int ,int> , int>>q ; 
+
+           q.push({{src , 0} , 0}) ; 
+           dis[src] = 0 ; 
 
 
-        vector<int>dis(n , INT_MAX) ; 
+           while(q.size()){
+                int u = q.front().first.first ; 
+                int wt = q.front().first.second ; 
 
-        queue<pair<pair<int ,int> , int>>q ; 
+                int stop = q.front().second ; 
+                q.pop() ; 
 
-        q.push({{src , 0} , 0}) ; 
-        dis[src] = 0 ; 
+                if(stop > k) continue ; 
 
-        while(q.size() > 0){
-            int s = q.front().first.first ; 
-            int w = q.front().first.second ; 
-            int stop = q.front().second ; 
-            q.pop() ;
+                for(auto val : adj[u]){
+                   int v = val.first ; 
+                   int w = val.second ; 
 
-            if(stop > k) continue ; 
-
-            for(auto val : adj[s]){
-                int sr = val.first ; 
-                int wt = val.second ; 
-
-                if(wt + w < dis[sr]){
-                    dis[sr] = wt + w ; 
-                    q.push({{sr , wt+w} , stop+ 1}) ;
+                    if(w + wt < dis[v]){
+                        dis[v] = w + wt ; 
+                        q.push({{v , w} , stop + 1}) ;
+                    }
                 }
-            }
-        }
-        if(dis[dst] == INT_MAX) return -1 ; 
+           }
+           return dis[dst] ; 
 
-        return dis[dst] ;
         
     }
 };

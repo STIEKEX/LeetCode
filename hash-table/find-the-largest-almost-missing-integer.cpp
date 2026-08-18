dbe850e703eb37cf498fa643b7410dd48337d935
Deepak Farswan <devfarswan56@gmail.com>
2026-08-18 17:33:40 +0530

    LC-find-the-largest-almost-missing-integer [Runtime: N/A; Memory: N/A]

--- hash-table/find-the-largest-almost-missing-integer.cpp
+++ hash-table/find-the-largest-almost-missing-integer.cpp
@@ -4,15 +4,19 @@ public:
         int n = nums.size() ; 
         unordered_map<int ,int>m  ;
 
+        int mx = INT_MIN ; 
         for(int val : nums){
             m[val]++ ; 
+            mx = max(mx , val) ; 
         }
+        if(n == k)return mx ; 
         int x = nums[0] ; 
 
         int y = nums[n-1] ;
 
         int f1 = m[x] ; 
         int f2 = m[y] ; 
+
 
         if(f1 == 1 && f2 ==1){
             return max(x , y) ; 

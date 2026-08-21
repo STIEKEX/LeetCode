2c5047f22a45858ff940e1502ab63799e46619c2
Deepak Farswan <devfarswan56@gmail.com>
2026-08-21 10:25:30 +0530

    LC-number-of-1-bits [Runtime: N/A; Memory: N/A]

--- bit-manipulation/number-of-1-bits.cpp
+++ bit-manipulation/number-of-1-bits.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-       
-        // long long pow = 1 ;
-        int count = 0 ;
-        while(n !=0){
-            int rem = n %2 ; 
-            if(rem == 1) {
-                count++ ; 
+        
+        int cnt = 0 ; 
+
+        while(n){
+            if(n&1){
+                cnt++  ;
             }
-            
-           
-            n = n/2 ;
-            // if(pow>INT_MAX/10){
-            //     pow = pow/10;
-
-            // }
-            // pow = pow * 10 ;
-
+            n = n >> 1 ; 
         }
-        return count ;
-
-            
-        
-        
+        return cnt ; 
     }
 };

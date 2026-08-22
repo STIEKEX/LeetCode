53a8e3d4117ce3a46f16883bb60ccf448dcc428c
Deepak Farswan <devfarswan56@gmail.com>
2026-08-22 22:11:21 +0530

    LC-check-divisibility-by-digit-sum-and-product [Runtime: N/A; Memory: N/A]

--- math/check-divisibility-by-digit-sum-and-product.cpp
+++ math/check-divisibility-by-digit-sum-and-product.cpp
@@ -5,14 +5,14 @@ public:
 
         long long  p = 1 ; 
         long long  sum = 0 ; 
-       
+       int temp  = n ; 
         while(n != 0 ){
             long long rem = n%10 ; 
             p *= rem  ;
             sum += rem ; 
             n = n/10  ;
         }
-        return n % (p + sum) == 0 ; 
+        return temp % (p + sum) == 0 ; 
         
         
     }

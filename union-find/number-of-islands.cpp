8d45b7e958914e43b3d3fa70e9c13fab944e0507
Deepak Farswan <devfarswan56@gmail.com>
2026-08-22 23:32:16 +0530

    LC-number-of-islands [Runtime: N/A; Memory: N/A]

--- union-find/number-of-islands.cpp
+++ union-find/number-of-islands.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-        void dfs(int i , int j , int m , int n , vector<vector<bool>>&seen , vector<vector<char>>&grid){
-            if(i < 0 || j < 0 || i>= m || j>= n || seen[i][j] || grid[i][j] != '1')return ; 
-            seen[i][j] = true ;
-            dfs(i-1 , j , m , n , seen , grid) ; 
-            dfs(i+1 , j , m , n , seen , grid) ; 
-            dfs(i , j+1 , m , n , seen , grid) ; 
-            dfs( i , j-1 , m  , n , seen , grid) ;
-        }
+    vector<vector<bool>>seen ; 
+    void dfs(int i , int j , int m , int n , vector<vector<char>>&grid){
+        
+        if(i < 0 || j < 0 || i >= m || j >=n|| grid[i][j] == '0' || seen[i][j] ) return ; 
+
+
+        seen[i][j] = true ;
+        dfs(i+1 , j , m , n , grid) ; 
+        dfs(i , j+1 , m , n , grid)  ;
+        dfs(i-1 , j , m,  n , grid) ; 
+        dfs(i , j-1 , m , n , grid) ; 
+
+
+
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size() ; 
+        
+
+        int cnt = 0 ; 
+
+        int m = grid.size()  ;
         int n = grid[0].size() ; 
 
-        vector<vector<bool>>seen(m , vector<bool>(n , false)) ;
-        int count = 0 ; 
+        seen.resize(m , vector<bool>(n , false)) ; 
+
         for(int i = 0 ; i<m ; i++){
-            for(int j = 0 ; j< n ; j++){
-                if(grid[i][j] == '1' && !seen[i][j]){
-                    dfs(i , j , m , n , seen , grid) ; 
-                    count++ ;
+            for(int j = 0 ; j<n ; j++){
+
+                if(grid[i][j] == '1'&& !seen[i][j]){
+                    cnt++ ; 
+                    dfs(i , j  , m , n , grid) ; 
                 }
             }
         }
-        return count ;
-        
+        return cnt ; 
+
     }
 };

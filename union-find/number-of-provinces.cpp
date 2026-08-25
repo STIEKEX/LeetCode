32c3b3cbebb7f9c0526a5283d579965c43cec30e
Deepak Farswan <devfarswan56@gmail.com>
2026-08-25 18:37:51 +0530

    LC-number-of-provinces [Runtime: N/A; Memory: N/A]

--- union-find/number-of-provinces.cpp
+++ union-find/number-of-provinces.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    void dfs(int i , vector<vector<int>>&adj, vector<bool>&seen){
 
-        if(seen[i]) return ; 
+    void dfs(int u , vector<vector<int>>&g , vector<bool>&seen){
 
-        seen[i] = true ; 
+        seen[u] = true ; 
+        int n = g.size() ; 
+        for(int v = 0 ; v<n ; v++){
 
-        for(int val : adj[i]){
-            if(!seen[val]){
-                dfs(val , adj , seen) ; 
+            if(g[u][v] == 1 && !seen[v]){
+                dfs(v , g , seen)  ;
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& grid) {
+    int findCircleNum(vector<vector<int>>& g) {
+        int m = g[0].size() ; 
+        int n = g.size() ; 
 
-        int n = grid.size() ; 
+      vector<bool>seen(n , false) ; 
 
-        vector<vector<int>>adj(n) ; 
-
-        vector<bool>seen(n) ; 
-
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<n ; j++){
-                
-                if(grid[i][j] == 1 && i!=j){
-
-                    adj[i].push_back(j) ; 
-                    adj[j].push_back(i) ; 
-                }
-            }
-        }
-
-        int count = 0 ; 
-        
-        for(int i = 0 ; i<n ; i++){
+        int cnt = 0 ; 
+        for(int i = 0 ; i< n ; i++){
 
             if(!seen[i]){
-                count++ ; 
-                dfs(i , adj , seen) ; 
+                cnt++ ; 
+                dfs(i , g , seen) ; 
             }
         }
-        return count ; 
+        return cnt ; 
         
     }
 };

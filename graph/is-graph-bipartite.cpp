910f8f2659e296f615f3cb6a035c2036f6f42b7c
Deepak Farswan <devfarswan56@gmail.com>
2026-08-25 18:24:48 +0530

    LC-is-graph-bipartite [Runtime: N/A; Memory: N/A]

--- graph/is-graph-bipartite.cpp
+++ graph/is-graph-bipartite.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-        bool bfs(vector<vector<int>>& graph , vector<int>&color){
-            int n = graph.size() ; 
+    bool bfs(vector<vector<int>>&graph , vector<int>&color){
 
-            for(int i = 0 ; i < n ; i++){
+        int n = graph.size() ; 
 
-                if(color[i] == -1){
-                    queue<int>q ; 
-                    q.push(i) ; 
-                    color[i] = 1 ; 
+        for(int i = 0 ; i<n ; i++){
 
-                    while(q.size() > 0){
-                        int curr = q.front() ; 
-                        q.pop() ; 
+            if(color[i] == -1){
 
-                        for(int val : graph[curr]){
-                            if(color[val] == -1){
-                                color[val] = 1 - color[curr] ; 
-                                q.push(val) ;
-                            }
-                            else if(color[val] == color[curr]) return false; 
+                queue<int>q ; 
+                q.push(i) ; 
+
+                color[i] = 1 ; 
+
+                while(q.size()){
+                    int u = q.front() ; 
+                    q.pop() ; 
+
+
+                    for(int val : graph[u]){
+                        if(color[val] == -1){
+                            color[val] = 1 - color[u] ; 
+                            q.push(val) ; 
                         }
+                        else if(color[val] == color[u]){
+                            return false ;
+                        }
+                       
                     }
                 }
+                
             }
-            return true ;
+            
         }
+        return true ;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
-        int m = graph.size() ; 
-        vector<int>color(m , -1) ; 
+        int n = graph.size() ; 
+        vector<int>color(n , -1) ; 
 
         return bfs(graph , color) ; 
-
         
     }
 };

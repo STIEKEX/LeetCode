da3f0519fd0df94aafdbe01a34cc46f75083ad44
Deepak Farswan <devfarswan56@gmail.com>
2026-08-30 21:06:27 +0530

    LC-removing-minimum-and-maximum-from-array [Runtime: N/A; Memory: N/A]

--- array/removing-minimum-and-maximum-from-array.cpp
+++ array/removing-minimum-and-maximum-from-array.cpp
@@ -21,16 +21,16 @@ public:
 
        int ans = INT_MAX  ;
 
-        ans = min(ans , max(mxI , mnI)) ; // left
+        ans = min(ans , max(mxI , mnI))+1 ; // left
         ans = min(ans , n - (min(mxI , mnI))) ;  // right ; 
 
         // left , right ; 
 
-        int t1 = mxI + (n-mnI) ; 
-        int t2 = mnI + (n-mxI) ; 
+        int t1 = mxI + (n-mnI) +1; 
+        int t2 = mnI + (n-mxI) +1; 
 
         ans = min(ans , min(t1 ,t2)) ; 
-        return ans+1 ;
+        return ans ;
 
 
 

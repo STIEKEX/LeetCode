bad963187d1ec7d14101e463c53bba38e7d507fe
Deepak Farswan <devfarswan56@gmail.com>
2026-08-30 21:04:34 +0530

    LC-removing-minimum-and-maximum-from-array [Runtime: N/A; Memory: N/A]

---
+++ greedy/removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size() ; 
+       int mn = INT_MAX ; 
+       int mx = INT_MIN  ;
+
+       int mnI =  0 ; 
+       int mxI = 0 ; 
+
+       for(int i = 0 ; i<nums.size() ; i++){
+            if(mn > nums[i]){
+                mn = nums[i] ; 
+                mnI =  i;
+            }
+            if(mx < nums[i]){
+                mx = nums[i] ; 
+                mxI = i ; 
+            }
+       }
+
+       int ans = INT_MAX  ;
+
+        ans = min(ans , max(mxI , mnI)) ; // left
+        ans = min(ans , n - (min(mxI , mnI))) ;  // right ; 
+
+        // left , right ; 
+
+        int t1 = mxI + (n-mnI) ; 
+        int t2 = mnI + (n-mxI) ; 
+
+        ans = min(ans , min(t1 ,t2)) ; 
+        return ans+1 ;
+
+
+
+        
+    }
+};

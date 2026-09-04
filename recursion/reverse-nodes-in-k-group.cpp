01aebfcecfa801071308cd4d43fa7565a408cd95
Deepak Farswan <devfarswan56@gmail.com>
2026-09-04 11:37:34 +0530

    LC-reverse-nodes-in-k-group [Runtime: N/A; Memory: N/A]

--- recursion/reverse-nodes-in-k-group.cpp
+++ recursion/reverse-nodes-in-k-group.cpp
@@ -10,36 +10,7 @@
  */
 class Solution {
 public:
-    
     ListNode* reverseKGroup(ListNode* head, int k) {
         
-
-       vector<int>arr ; 
-       ListNode* temp = head ;
-       while(temp != NULL){
-        arr.push_back(temp->val) ; 
-        temp = temp->next ;
-       }
-
-       int i = 0 ; 
-        int j = k-1;
-       while(j< arr.size()){
-
-        reverse(arr.begin() + i , arr.begin() + j+1) ; 
-        i = j+1 ; 
-        j += k ; 
-       }
-
-       ListNode* newNode = new ListNode(0) ; 
-        ListNode* newhead = newNode ; 
-       for(int val : arr){
-        newNode->next =  new ListNode(val) ; 
-        newNode = newNode->next ; 
-       }
-       return newhead->next ; 
-
-
-
-
     }
 };

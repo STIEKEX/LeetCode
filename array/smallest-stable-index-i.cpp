085c55a77cc188c341ce4fcf8555f2ddad8c1813
Deepak Farswan <devfarswan56@gmail.com>
2026-09-04 10:21:57 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-i.cpp
+++ array/smallest-stable-index-i.cpp
@@ -4,28 +4,26 @@ public:
 
         int n = nums.size() ; 
 
-        int mx = nums[0] ;
 
-        
-        
+        int res = INT_MAX ; 
 
-        int idx = -1 ;
+        vector<int>suffix(n ) ; 
+        suffix[n-1 ] = nums[n-1] ; //min ; 
+
+        for(int i = n-2 ;i>= 0 ; i--){
+            suffix[i] = min(nums[i] , suffix[i+1]) ; 
+        }
+
+        int mx  =INT_MIN ; 
+
         for(int i = 0 ; i<n ; i++){
-
             mx = max(mx , nums[i]) ; 
-            int mn = INT_MAX ; 
 
-            for(int k = i ; k<n ; k++){
-                mn = min(mn , nums[k]) ; 
-            }
-            int val = mx - mn ; 
-            if(val <= k ){
+            if(mx - suffix[i] <= k){
                 return i ; 
             }
         }
-        return idx; 
-
-        
+        return -1 ;
         
     }
 };

fcacb1acf1263fab7f8aa12b9cf9b9311c0d5d18
Deepak Farswan <devfarswan56@gmail.com>
2026-09-05 08:01:18 +0530

    LC-smallest-stable-index-ii [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-ii.cpp
+++ array/smallest-stable-index-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-
         int n = nums.size() ; 
 
-        int mx = nums[0] ; 
-    
+        vector<int>suffix(n) ; 
+
+        suffix[n-1] = nums[n-1] ; 
+        for(int i = n -2 ; i>=0 ; i--){
+
+            suffix[i] = min(nums[i] , suffix[i+1]) ; 
+        }
+
+        int ans = 0 ; 
+        int mx = INT_MIN  ; 
+        for(int i =0  ; i<n ; i++){
+                mx = max(mx , nums[i])  ;
+
+                if(mx - suffix[i] <= k){
+                    return i  ; 
+                }
+        }
+        return -1 ; 
         
-        vector<int>mn(n) ;
-        mn[n-1] = nums[n-1] ; 
-        int mni = nums[n-1] ; 
-        for(int i = n-2 ; i>= 0 ; i--){
-            mni = min(mni , nums[i]) ;
-            mn[i] = mni ;
-            
-        }
-
-        for(int i = 0 ; i<n ; i++){
-            mx = max(mx, nums[i]) ; 
-            mni = mn[i] ; 
-
-            int val = mx - mni ;
-            if(val <= k){
-                return i ;
-            }
-        }
-        return -1 ;
     }
 };

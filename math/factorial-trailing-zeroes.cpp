d1858b7c8b42132f5238f88946d7fac7a16edad9
Deepak Farswan <devfarswan56@gmail.com>
2026-09-05 20:05:28 +0530

    LC-factorial-trailing-zeroes [Runtime: N/A; Memory: N/A]

---
+++ math/factorial-trailing-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+
+
+        int ans = 0 ;
+
+        while(n > 0){
+
+            n = n/5 ; 
+            ans += n ; 
+        }
+        // cout << ans <<endl ;
+        return ans ; 
+        
+    }
+};

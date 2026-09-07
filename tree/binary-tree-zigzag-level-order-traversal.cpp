8eb03e066bfe42c8df4649d3399b52a151a0ebc1
Deepak Farswan <devfarswan56@gmail.com>
2026-09-07 08:52:47 +0530

    LC-binary-tree-zigzag-level-order-traversal [Runtime: N/A; Memory: N/A]

--- tree/binary-tree-zigzag-level-order-traversal.cpp
+++ tree/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,41 +12,46 @@
 class Solution {
 public:
     vector<vector<int>>ans ; 
-
     void bfs(TreeNode* root){
         if(root == NULL) return ; 
+        bool even = false ; 
 
         queue<TreeNode*>q ; 
         q.push(root) ; 
-
-        int level = 1 ; 
+        
         while(!q.empty()){
             int l = q.size()  ;
-
             vector<int>arr ; 
-
             for(int i = 0 ; i<l ; i++){
-                TreeNode* curr = q.front() ; 
-                q.pop()  ;
 
-                arr.push_back(curr->val)  ;
+                TreeNode* curr = q.front()  ;
+                q.pop() ; 
+
+                arr.push_back(curr->val) ; 
+
                 if(curr->left) q.push(curr->left) ; 
-                if(curr->right) q.push(curr->right) ; 
+                if(curr->right) q.push(curr->right) ;         
+
             }
-            if(level % 2 == 1){
-                ans.push_back(arr) ; 
-            }
-            else{
+
+            if(even){
                 reverse(arr.begin() , arr.end()) ; 
                 ans.push_back(arr) ; 
+                even = false ;
             }
-            level++  ;
+            else{
+                ans.push_back(arr) ; 
+                even = true ;
+            }
+            
+
         }
 
+        
     }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
 
-        bfs(root)  ;
+        bfs(root) ; 
         return ans ;
         
     }

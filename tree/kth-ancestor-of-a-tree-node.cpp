aa85ed38d1aeaeb5528a24e0ee9613f10fdfa10b
Deepak Farswan <devfarswan56@gmail.com>
2026-09-11 18:39:44 +0530

    LC-kth-ancestor-of-a-tree-node [Runtime: N/A; Memory: N/A]

---
+++ tree/kth-ancestor-of-a-tree-node.cpp
@@ -0,0 +1,48 @@
+class TreeAncestor {
+public:
+    vector<vector<int>>an ; 
+    int row ;
+    int col ;
+    TreeAncestor(int n, vector<int>& parent) {
+        row = n ; 
+        col = log2(n)+1 ; 
+        an.resize(n , vector<int>(col , -1)) ; 
+
+        for(int i = 0 ; i<n ; i++){
+            an[i][0] = parent[i] ; 
+        }
+
+        for(int i = 1 ; i<col ; i++){
+
+            for(int j = 0 ; j<n ; j++){
+
+                if(an[j][i-1] != -1){
+                    an[j][i] = an[an[j][i-1]][i-1] ; 
+                }
+            }
+        }
+        
+    }
+    
+    int getKthAncestor(int node, int k) {
+
+    for(int i = 0 ; i<col ; i++){
+
+        if(k & (1 << i)){
+            node = an[node][i] ; 
+
+            if(node == -1) return -1 ;
+        }
+        
+    }
+    return node ;
+        
+        
+    }
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */

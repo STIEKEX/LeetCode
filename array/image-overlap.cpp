77bc8a5c76639eba0411c2b74ab4d4b2af27ff2c
Deepak Farswan <devfarswan56@gmail.com>
2026-09-13 12:00:01 +0530

    LC-image-overlap [Runtime: N/A; Memory: N/A]

--- array/image-overlap.cpp
+++ array/image-overlap.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int n = img1.size() ;
+        int n = img1.size() ; 
         int cnt1 = 0 ; 
-        int cnt2 = 0 ;
+        int cnt2 = 0 ; 
 
         for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<n ; j++){
 
+            for(int j = 0;  j<n ;j ++){
 
+                if(img1[i][j] == 1) cnt1++  ;
                 if(img2[i][j] == 1)cnt2++ ; 
-                if(img1[i][j] == 1)cnt1++ ; 
+
+
             }
         }
 
-       
-        if(cnt1 == 0 || cnt2 == 0) return 0 ; 
+        return min(cnt1, cnt2) ;
 
-        if(cnt1 < cnt2) return cnt1 ; 
-        return cnt2 ; 
 
+        
     }
 };

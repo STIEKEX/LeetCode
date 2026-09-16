9902380b28a85d25dfe42ff449c9f8e856ac0905
Deepak Farswan <devfarswan56@gmail.com>
2026-09-16 18:27:13 +0530

    LC-next-greater-element-i [Runtime: N/A; Memory: N/A]

---
+++ monotonic-stack/next-greater-element-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        int size = nums1.size() ; 
+        int n = nums2.size() ; 
+        unordered_map<int ,int>m ; 
+
+        stack<int>s ; 
+
+       
+        for(int i = n-1 ; i>=0 ; i--){
+
+            while(!s.empty() && s.top() <= nums2[i]){
+                s.pop() ; 
+            }
+            if(s.empty()){
+                m[nums2[i]] = -1 ;
+            }
+            else{
+                m[nums2[i]] = s.top() ; 
+            }
+            s.push(nums2[i])  ;
+         
+        }
+
+        vector<int>ans ; 
+        for(int i =0  ; i<size ; i++){
+
+            ans.push_back(m[nums1[i]]) ; 
+        }
+        return ans; 
+        
+
+    }
+};

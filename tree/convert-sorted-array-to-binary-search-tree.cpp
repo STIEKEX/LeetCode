61ebe27ae419a9a68b6b85b188c84a148c967ce3
Deepak Farswan <devfarswan56@gmail.com>
2026-09-18 20:44:45 +0530

    LC-convert-sorted-array-to-binary-search-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(int st , int end , vector<int>&nums){
+
+        if(st > end) return NULL ; 
+
+        int mid = st + (end-st)/2 ; 
+
+        TreeNode* root = new TreeNode(nums[mid]) ; 
+
+        root->left = solve(st , mid-1 , nums) ; 
+        root->right = solve(mid+1 , end , nums) ; 
+        return root ; 
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size() ; 
+        return solve(0 , n-1 , nums) ; 
+        
+    }
+};

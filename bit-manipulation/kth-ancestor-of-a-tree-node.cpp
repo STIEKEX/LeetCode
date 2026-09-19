2713029fda0c6cbf19cb106de5e2f775c6c98166
Deepak Farswan <devfarswan56@gmail.com>
2026-09-19 11:01:46 +0530

    LC-kth-ancestor-of-a-tree-node [Runtime: N/A; Memory: N/A]

--- bit-manipulation/kth-ancestor-of-a-tree-node.cpp
+++ bit-manipulation/kth-ancestor-of-a-tree-node.cpp
@@ -1,25 +1,27 @@
 class TreeAncestor {
 public:
-    vector<vector<int>>an ;
-    int row ;
-    int col ;   
+    /*
+    if we have to make 2 to the power j jump we will make
+    */
+    vector<vector<int>>an ; // if i take 2 to the power j jump form the node which node i will reach that info we will store in this 2d array : row mean node and col mean jump 
+    int row ; 
+    int col ; 
     TreeAncestor(int n, vector<int>& parent) {
+         row = n ; 
+        col = log2(n) +1;  // how many col we need to get n  2 to power j = n  so 
 
-        row = n ; 
-        col = log2(n)+1 ; 
-        an.resize(row , vector<int>(col , -1))  ;
+        an.resize(row , vector<int>(col, -1)) ; 
 
-        for(int i = 0 ; i<n  ; i++){
-            an[i][0] = parent[i] ; 
+        for(int i = 0 ; i<n ; i++){
+            an[i][0]= parent[i] ; 
         }
 
         for(int j = 1 ; j<col ; j++){
 
             for(int i = 0 ; i<row ; i++){
-                
-                if(an[i][j-1] != -1) {
-                an[i][j] = an[an[i][j-1]][j-1] ; 
-                }
+                if(an[i][j-1] != -1){
+                    an[i][j] = an[an[i][j-1]][j-1] ; 
+                } // if we have to make 2 to the power j jump -> we will make 2 to the power j-1 + 2 to the power j-1 jump
             }
         }
         
@@ -27,10 +29,11 @@ public:
     
     int getKthAncestor(int node, int k) {
 
-        for(int i = 0 ; i<col ; i++){
+        for(int i = 0;  i<col ; i++){
 
-            if(k & (1 << i)){
+            if(k & (1 << i)){ // if i th bit of k is set or not ? 
                 node = an[node][i] ; 
+
                 if(node == -1) return -1 ;
             }
         }
